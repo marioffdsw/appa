51eb453cb1807ccef62d987ecfdb428ff2f59b5e
marioffdsw <marioffdsw@gmail.com>
2015-03-25 11:39:27 -0500

    Configuracion y funcion de cambio de fragments listo, aun noesta el layout de seleccion de fragments

---
+++ app/src/main/java/com/paolosport/appa/PersonaFragment.java
@@ -0,0 +1,49 @@
+package com.paolosport.appa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PersonaFragment extends Fragment {
+
+    // private OnFragmentInteractionListener mListener;
+
+    public PersonaFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    } // fin del metodo onCreate
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_persona, container, false);
+    } // fin del metodo onCreateView
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        /*
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        } // fin catch
+        */
+    } // fin del metodo onAttach
+
+    /*
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    } // fin de la Interface OnFragmentInteractionListener */
+} // fin de la clase LocalFragment
