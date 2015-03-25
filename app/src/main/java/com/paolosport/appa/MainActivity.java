f0c90b08f5067fc2ccd81a458e5c6045fbad075c
andi9010 <andres_solarteo@hotmail.com>
2015-03-24 21:07:24 -0500

    comit del 24 marzo

--- app/src/main/java/com/paolosport/appa/MainActivity.java
+++ app/src/main/java/com/paolosport/appa/MainActivity.java
@@ -1,19 +1,26 @@
 package com.paolosport.appa;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.SpinnerAdapter;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
+import com.paolosport.appa.activities.ActivityConfiguracion;
 import com.paolosport.appa.activities.ActivityListaPrestamos;
 import com.paolosport.appa.activities.ActivityLocal;
 import com.paolosport.appa.activities.ActivityMarca;
 import com.paolosport.appa.activities.ActivityPersona;
+import com.paolosport.appa.activities.prueba;
 import com.paolosport.appa.persistencia.AdminSQLiteOpenHelper;
 import com.paolosport.appa.persistencia.dao.LocalDAO;
 
@@ -22,6 +29,8 @@ public class MainActivity extends ActionBarActivity {
 
     AdminSQLiteOpenHelper helper;
     LocalDAO localDAO;
+    int id=0;
+    Dialog customDialog=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +39,54 @@ public class MainActivity extends ActionBarActivity {
 
         helper = new AdminSQLiteOpenHelper( this );
         localDAO = new LocalDAO( this, helper );
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.logo_appa);
+        actionBar.setDisplayUseLogoEnabled(true);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.configuracion:
+                Intent i = new Intent(this, ActivityConfiguracion.class);
+                startActivity(i);
+                break;
+            case R.id.acercade:
+                mostrar(findViewById(id));
+                break;
+            case R.id.Ayuda:
+                Intent a = new Intent(this, prueba.class);
+                startActivity(a);
+               break;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void mostrar(View v)
+    {
+        customDialog = new Dialog(this);
+        //deshabilitamos el título por defecto
+        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //obligamos al usuario a pulsar los botones para cerrarlo
+        customDialog.setCancelable(false);
+        //establecemos el contenido de nuestro dialog
+        customDialog.setContentView(R.layout.dialogo_personal);
+
+        customDialog.show();
+        ((Button) customDialog.findViewById(R.id.aceptar)).setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view){customDialog.dismiss();}
+        });
     }
 
     /** el metodo local, lanza una activity que permite introducir
