3355f0ddf4ebc882293ac33f350c243c581fde80
andi9010 <andres_solarteo@hotmail.com>
2015-04-16 20:58:56 -0500

    union tabs

--- app/src/main/java/com/paolosport/appa/MainActivity.java
+++ app/src/main/java/com/paolosport/appa/MainActivity.java
@@ -5,17 +5,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
-import android.graphics.Shader;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
@@ -25,11 +14,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.view.View.OnClickListener;
-import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.paolosport.appa.activities.ActivityConfiguracion;
@@ -37,221 +23,58 @@ import com.paolosport.appa.activities.ActivityListaPrestamos;
 import com.paolosport.appa.activities.ActivityLocal;
 import com.paolosport.appa.activities.ActivityMarca;
 import com.paolosport.appa.activities.ActivityPersona;
-import com.paolosport.appa.activities.opcion_informacion;
 import com.paolosport.appa.fragments.PrestamoFormFragment;
 import com.paolosport.appa.fragments.PrestamoLstFragment;
-import com.paolosport.appa.persistencia.AdminSQLiteOpenHelper;
-import com.paolosport.appa.persistencia.dao.LocalDAO;
-import com.paolosport.appa.persistencia.dao.MarcaDAO;
-import com.paolosport.appa.persistencia.dao.PersonaDAO;
-import com.paolosport.appa.persistencia.entities.Local;
-import com.paolosport.appa.persistencia.entities.Marca;
-import com.paolosport.appa.persistencia.entities.Persona;
-import com.paolosport.appa.spinnerLocalPaquete.SpinnerAdapterLocal;
-import com.paolosport.appa.spinnerMarcaPaquete.SpinnerAdapterMarca;
-import com.paolosport.appa.spinnerPersonaPaquete.SpinnerAdapterPersonaLista;
-
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class MainActivity extends ActionBarActivity {
 
-    AdminSQLiteOpenHelper helper;
 
     int id=0;
     Dialog customDialog=null;
     public boolean sesion;
-    private ImageView imageView;
-
-    MarcaDAO marcaDAO;
-    LocalDAO localDAO;
-    PersonaDAO personaDAO;
-
-    HorizontalListView lv_persona;
-
-    private Spinner sp_marca,   sp_local, sp_persona;
-    String id_marca,url_marca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
         sesion=preferences.getBoolean("sesion",sesion);
 
-        helper = new AdminSQLiteOpenHelper( this );
-        localDAO = new LocalDAO( this, helper );
-        marcaDAO = new MarcaDAO(this,helper);
-        personaDAO = new PersonaDAO(this,helper);
+
         // setup action bar for tabs
         ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         actionBar.setDisplayShowTitleEnabled(false);
+
+        ActionBar.Tab tab = actionBar.newTab()
+                .setText(R.string.frm_prestamos)
+                .setTabListener(new TabListener<PrestamoFormFragment>(
+                        this, "artist", PrestamoFormFragment.class));
+        actionBar.addTab(tab);
+
+        tab = actionBar.newTab()
+                .setText(R.string.lst_prestamos)
+                .setTabListener(new TabListener<PrestamoLstFragment>(
+                        this, "album", PrestamoLstFragment.class));
+        actionBar.addTab(tab);
+        actionBar.setLogo( R.drawable.logo_appa );
+        actionBar.setDisplayShowTitleEnabled( true );
+        actionBar.setDisplayShowTitleEnabled( true );
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
 
         if(sesion==true){
             Toast.makeText(getApplicationContext(), "Sesión inicada", Toast.LENGTH_SHORT).show();
         }else{
             Toast.makeText(getApplicationContext(), "Sesión cerrada", Toast.LENGTH_SHORT).show();
         }
-        DatosPorDefecto();
-        CargarDatosListaPersonas();
-
-
-        /*imageView = (ImageView) findViewById(R.id.imageView1);
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.catico);
-        Bitmap b = Bitmap.createScaledBitmap(bitmap,100,100,true);
-        bitmap=b;
-
-        if(bitmap!=null)
-
-        {
-
-            int targetWidth = 65;
-            int targetHeight = 65;
-            Bitmap targetBitmap = Bitmap.createBitmap(targetWidth, targetHeight,Bitmap.Config.ARGB_8888);
-            BitmapShader shader;
-            shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-
-            Paint paint = new Paint();
-            paint.setAntiAlias(true);
-            paint.setShader(shader);
-            Canvas canvas = new Canvas(targetBitmap);
-            Path path = new Path();
-            path.addCircle(((float) targetWidth - 1) / 2,
-                    ((float) targetHeight - 1) / 2,
-                    (Math.min(((float) targetWidth),((float) targetHeight)) / 2),Path.Direction.CCW);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.clipPath(path);
-            Bitmap sourceBitmap = bitmap;
-            canvas.drawBitmap(sourceBitmap, new Rect(0, 0, sourceBitmap.getWidth(),sourceBitmap.getHeight()),
-                    new Rect(0, 0, targetWidth,targetHeight), null);
-
-
-            imageView.setImageBitmap(targetBitmap);   //set the circular image to your imageview
-        }
-       /* else
-        {
-            queuePhoto(url, imageView);
-            imageView.setImageResource(stub_id);
-        }*/
     }
 
 
     //?????????????????????????????????????????????????????????????????????????
     //?????????????????????????????????????????????????????????????????????????
-    private void DatosPorDefecto() {
-        marcaDAO.open();
-        localDAO.open();
-
-        final ArrayList<Marca> listaMarcas = marcaDAO.retrieveAll();
-        final ArrayList<Local> listaLocales = localDAO.retrieveAll();
-
-
-        sp_marca= (android.widget.Spinner)findViewById(R.id.sp_marca_prestamo);
-        sp_local= (android.widget.Spinner)findViewById(R.id.sp_local_prestamo);
-
-        sp_marca.setAdapter(new SpinnerAdapterMarca(this,listaMarcas));
-        sp_local.setAdapter(new SpinnerAdapterLocal(this,listaLocales));
-
-        marcaDAO.close();
-        localDAO.close();
-
-        sp_marca.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
-            {
-                id_marca=((Marca) adapterView.getItemAtPosition(position)).getId();
-                url_marca=((Marca) adapterView.getItemAtPosition(position)).getUrl();
-
-                //et_nombre_marca_editado.setText(((Marca) adapterView.getItemAtPosition(position)).getNombre());
-
-                String url = url_marca.toString();
-
-                Bitmap bmImg = BitmapFactory.decodeFile(listaMarcas.get(position).getUrl());
-                Pattern pat = Pattern.compile(".*/.*");
-                Matcher mat = pat.matcher(url);
-
-                /*if (mat.matches()) {
-                    iv_editada.setImageBitmap(bmImg);
-                } else {
-                    int path = getResources().getIdentifier(url,"drawable", "com.paolosport.appa");
-                    iv_editada.setImageResource(path);
-                }*/
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView){}
-        });
-
-        sp_local.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
-                //id_marca=((Marca) adapterView.getItemAtPosition(position)).getId();
-                //url_marca=((Marca) adapterView.getItemAtPosition(position)).getUrl();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
-
-
-    }//end method DatosPorDefecto
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -273,8 +96,8 @@ public class MainActivity extends ActionBarActivity {
                 mostrar(findViewById(id));
                 break;
             case R.id.Ayuda:
-                Intent a = new Intent(this, opcion_informacion.class);
-                startActivity(a);
+                //Intent a = new Intent(this, opcion_informacion.class);
+                //startActivity(a);
                break;
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/paolosport/appa/fragments/PrestamoFormFragment.java
+++ app/src/main/java/com/paolosport/appa/fragments/PrestamoFormFragment.java
@@ -1,33 +1,150 @@
 package com.paolosport.appa.fragments;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.paolosport.appa.R;
+import com.paolosport.appa.persistencia.AdminSQLiteOpenHelper;
+import com.paolosport.appa.persistencia.dao.LocalDAO;
+import com.paolosport.appa.persistencia.dao.MarcaDAO;
+import com.paolosport.appa.persistencia.dao.PersonaDAO;
+import com.paolosport.appa.persistencia.entities.Local;
+import com.paolosport.appa.persistencia.entities.Marca;
+import com.paolosport.appa.persistencia.entities.Persona;
+import com.paolosport.appa.spinnerLocalPaquete.SpinnerAdapterLocal;
+import com.paolosport.appa.spinnerMarcaPaquete.SpinnerAdapterMarca;
+import com.paolosport.appa.spinnerPersonaPaquete.SpinnerAdapterPersonaLista;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PrestamoFormFragment extends Fragment {
+
+    View view;
+
+    private Spinner sp_marca,   sp_local, sp_persona;
+    String id_marca,url_marca;
+
+
+    private ImageView imageView;
+
+    AdminSQLiteOpenHelper helper;
+    MarcaDAO marcaDAO;
+    LocalDAO localDAO;
+    PersonaDAO personaDAO;
 
     public PrestamoFormFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate( Bundle savedInstanceState ) {
+    public void onCreate( Bundle savedInstanceState )
+    {
         super.onCreate( savedInstanceState );
+        super.onActivityCreated( savedInstanceState );
+
+        if(view != null){
+        }
     }
 
+
     @Override
-    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState ) {
+
+
+
+        helper     = new AdminSQLiteOpenHelper( getActivity().getApplicationContext() );
+        localDAO   = new LocalDAO( getActivity().getApplicationContext(), helper );
+        marcaDAO   = new MarcaDAO( getActivity().getApplicationContext(), helper );
+        personaDAO = new PersonaDAO( getActivity().getApplicationContext(), helper );
+        view= inflater.inflate(R.layout.fragment_prestamo_form, container, false);
+
+        DatosPorDefecto();
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_prestamo_form, container, false );
+
+        return view;
     }
 
     @Override
     public void onAttach( Activity activity ) {
         super.onAttach( activity );
     }
+
+    private void DatosPorDefecto() {
+        marcaDAO.open();
+        localDAO.open();
+        personaDAO.open();
+
+        final ArrayList<Marca> listaMarcas = marcaDAO.retrieveAll();
+        final ArrayList<Local> listaLocales = localDAO.retrieveAll();
+        final ArrayList<Persona> listaPersonas = personaDAO.retrieveAll();
+
+
+        sp_marca= (android.widget.Spinner)view.findViewById(R.id.sp_marca_prestamo);
+        sp_local= (android.widget.Spinner)view.findViewById(R.id.sp_local_prestamo);
+        sp_persona= (android.widget.Spinner)view.findViewById(R.id.sp_persona_prestamo);
+
+        sp_marca.setAdapter(new SpinnerAdapterMarca(getActivity().getApplicationContext(),listaMarcas));
+        sp_local.setAdapter(new SpinnerAdapterLocal(getActivity().getApplicationContext(),listaLocales));
+        sp_persona.setAdapter(new SpinnerAdapterPersonaLista(getActivity().getApplicationContext(),listaPersonas));
+
+        marcaDAO.close();
+        localDAO.close();
+        personaDAO.close();
+
+        sp_marca.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                id_marca=((Marca) adapterView.getItemAtPosition(position)).getId();
+                url_marca=((Marca) adapterView.getItemAtPosition(position)).getUrl();
+
+                //et_nombre_marca_editado.setText(((Marca) adapterView.getItemAtPosition(position)).getNombre());
+
+                String url = url_marca.toString();
+
+                Bitmap bmImg = BitmapFactory.decodeFile(listaMarcas.get(position).getUrl());
+                Pattern pat = Pattern.compile(".*/.*");
+                Matcher mat = pat.matcher(url);
+
+                /*if (mat.matches()) {
+                    iv_editada.setImageBitmap(bmImg);
+                } else {
+                    int path = getResources().getIdentifier(url,"drawable", "com.paolosport.appa");
+                    iv_editada.setImageResource(path);
+                }*/
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView){}
+        });
+
+        sp_local.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                //id_marca=((Marca) adapterView.getItemAtPosition(position)).getId();
+                //url_marca=((Marca) adapterView.getItemAtPosition(position)).getUrl();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+
+    }//end method DatosPorDefecto
+
 }
